44dc50e1a32f366a2f4965b69c1326f98ae434f2
0xpics <1CF1DF@proton.me>
2026-01-15 10:04:11 -0300

    Ajustando app.ts e app.html para mostrar o botao

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.html', // Ele vai ler o app.html que limpamos acima
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent {
+  private http = inject(HttpClient);
+  dados = signal<any>(null);
+
+  chamarApi() {
+    const url = 'https://mympqg08a4.execute-api.us-east-1.amazonaws.com/data';
+    
+    this.http.get(url).subscribe({
+      next: (res) => {
+        this.dados.set(res);
+      },
+      error: (err) => {
+        console.error('Erro na chamada:', err);
+        alert('Erro ao conectar com a AWS. Verifique o console (F12).');
+      }
+    });
+  }
 }
